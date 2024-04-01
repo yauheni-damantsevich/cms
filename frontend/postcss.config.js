b2c9177c362bf7c240a09ba57adf390b01e56c80
yauheni-damantsevich <yauheni.damantsevich@outlook.com>
2024-04-01 12:36:38 +0300

    initial commit

---
+++ frontend/postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+const config = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
